78846d32aff9e33eef0ffb2790fbe80ef2882e9b
Harshit saxena <saxena24harshit@gmail.com>
2025-02-05 15:11:40 +0530

    fix vite permission denied

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from './component/pages/BookingsPage.jsx'
 import BookingPage from './component/pages/BookingPage.jsx'
 import QueryPage from './component/pages/QueryPage.jsx'
 
-axios.defaults.baseURL='https://airbnb-backend-fwxz.onrender.com/api'
+axios.defaults.baseURL= 'https://airbnb-backend-fwxz.onrender.com ' ||'http://localhost:4000'
 axios.defaults.withCredentials = true
 
 function App() {
