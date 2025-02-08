018ff1a105f4459b7603944eddc70e46fb5cebd0
Harshit saxena <saxena24harshit@gmail.com>
2025-02-09 03:46:06 +0530

    cros url correction

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/uploads", express.static(uploadDir));
 app.use(
   cors({
     credentials: true,
-    origin:[process.env.vite_frontend_url]
+    origin:["http://localhost:5173"]
   })
 );
 
