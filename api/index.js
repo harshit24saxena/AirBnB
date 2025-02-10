23448f0f5ba02ad897a8fe9552b883087bd1a3f5
Harshit saxena <saxena24harshit@gmail.com>
2025-02-11 01:44:44 +0530

    cors origin error correction

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static(uploadDir));
 app.use(
   cors({
-    origin:[process.env.VITE_FRONTEND_URL ],
+    origin:process.env.VITE_FRONTEND_URL ,
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
