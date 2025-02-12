43f753045af8e0842b812cc7aa832d0387ac2754
Harshit saxena <saxena24harshit@gmail.com>
2025-02-13 04:12:12 +0530

    updating app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from './component/pages/BookingsPage.jsx'
 import BookingPage from './component/pages/BookingPage.jsx'
 import QueryPage from './component/pages/QueryPage.jsx'
 
-axios.defaults.baseURL="http://localhost:4000/"
+axios.defaults.baseURL=import.meta.env.VITE_BACKEND_URL
 axios.defaults.withCredentials = true
 
 function App() {
